64c5c086912b2da1edbec103b93d63c83d5bae13
Clara Bernadou <clarabernadou56690@gmail.com>
2023-04-18 00:01:18 +0200

    If location ID is not correct, redirect to the location's page

--- client/src/pages/location/Location.jsx
+++ client/src/pages/location/Location.jsx
@@ -2,13 +2,21 @@ import Header from "../../components/header/Header.jsx"
 import LocationInfo from "../../components/locationInfo/LocationInfo.jsx"
 import Footer from "../../components/footer/Footer.jsx"
 
+import NotFound from "../notFound/NotFound.jsx";
+
 export default function Location({location}) {
     console.log(location);
-    return (
-        <>
-            <Header/>
-                <LocationInfo location={location} />
-            <Footer />
-        </>
-      );
+    if(location == undefined){
+        return (
+            <NotFound />
+        );
+    }else{
+        return (
+            <>
+                <Header/>
+                    <LocationInfo location={location} />
+                <Footer />
+            </>
+        );
+    }
 }
